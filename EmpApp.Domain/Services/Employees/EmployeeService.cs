93b76923fee97e0fd68b57c56a3adf1046828a22
w3lel <66215630+w3lel@users.noreply.github.com>
2020-12-19 05:57:23 +0300

    Add service

---
+++ EmpApp.Domain/Services/Employees/EmployeeService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CyberAge.Database;
+using CyberAge.Entities;
+
+namespace CyberAge.Domain.Services.Employees
+{
+    public class EmployeeService
+    {
+        private readonly DatabaseContext _dbContext;
+        public EmployeeService(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Employee GetEmployee(Guid id)
+        {
+            return _dbContext.Employees.First(e => e.Id == id);
+        }
+
+        public List<Employee> GetEmployee()
+        {
+            return _dbContext.Employees.ToList();
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            var editableEmployee = employee;
+            editableEmployee.Id = Guid.NewGuid();
+            _dbContext.Add(editableEmployee);
+            _dbContext.SaveChanges();
+        }
+
+        public void RemoveEmployee(Guid id)
+        {
+            var employee = GetEmployee(id);
+            _dbContext.Remove(employee);
+            _dbContext.SaveChanges();
+        }
+
+        public void EditEmployee(Employee employee)
+        {
+            var editableEmployee = employee;
+            editableEmployee.Id = employee.Id;
+            editableEmployee.FullName = employee.FullName;
+            editableEmployee.DeviceDate = employee.DeviceDate;
+            editableEmployee.HoursNumber = employee.HoursNumber;
+            editableEmployee.Tasks = employee.Tasks;
+            editableEmployee.Role = employee.Role;
+            _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ EmpApp.Domain/Services/Ticket/TicketService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CyberAge.Database;
+using CyberAge.Entities;
+
+namespace CyberAge.Domain.Services.Tasks
+{
+    public class TicketService
+    {
+        private readonly DatabaseContext _dbContext;
+        public TicketService(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Ticket GetTasks(Guid id)
+        {
+            return _dbContext.Tickets.First(e => e.Id == id);
+        }
+
+        public List<Ticket> GetTasks()
+        {
+            return _dbContext.Tickets.ToList();
+        }
+
+        public void AddTicket(Ticket tickets)
+        {
+            var editableTicket = tickets;
+            editableTicket.Id = Guid.NewGuid();
+            _dbContext.Add(editableTicket);
+            _dbContext.SaveChanges();
+        }
+
+        public void RemoveTicket(Guid id)
+        {
+            var tasks = GetTasks(id);
+            _dbContext.Remove(tasks);
+            _dbContext.SaveChanges();
+        }
+
+        public void EditTasks(Ticket ticket)
+        {
+            var editableTicket = ticket;
+            editableTicket.Id = ticket.Id;
+            editableTicket.Name = ticket.Name;
+            editableTicket.Description= ticket.Description;
+            editableTicket.PresentableId = ticket.PresentableId;
+            _dbContext.SaveChanges();
+        }
+    }
+}
